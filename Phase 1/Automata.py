ad80456b6aaa288703ad7698653513daefa87ecf
MehrazinM <mehrazin82@gmail.com>
2025-04-16 00:32:53 +0330

    initial commit

---
+++ Phase 1/Automata.py
@@ -0,0 +1,13 @@
+class State:
+    def __init__(self):
+        pass
+
+
+
+class Transition:
+    def __init__(self):
+        pass
+    
+class FinalState(State):
+    def __init__(self):
+        pass
---
+++ Phase 1/InputReader.py
@@ -0,0 +1,55 @@
+class InputReader:
+    def __init__(self, filename = "input.txt", buffer_size = 100):
+        
+        self.filename = filename
+        self.buffer_pointer = 0
+        self.buffer_size = buffer_size
+        self.buffer = ""
+        self.line_no = 1
+        self.input_file = open(filename, "r")
+        self.__load_buffer()
+        
+    def __load_buffer(self):
+        
+        self.buffer = self.input_file.read(self.buffer_size)
+        if len(self.buffer) < self.buffer_size:
+            self.buffer += chr(26)
+            
+        self.buffer_pointer = 0
+        
+        
+    def next_char_exists(self):
+        if self.buffer_pointer < len(self.buffer):
+            return True
+
+        if len(self.buffer) < self.buffer_size:
+            return False
+
+        self._refill_buffer()
+        return self.has_next()
+
+    def get_line_no(self):
+        return self.line_no
+        
+    def push_back(self, char):
+        if char == '\n':
+            self.line_no -= 1
+
+        if self.buffer_pointer > 0:
+            self.buffer_pointer -= 1
+        else:
+            self.buffer = char + self.buffer
+            
+    def get_next_char(self):
+        if self.buffer_pointer >= len(self.buffer):
+            self._refill_buffer()
+
+        next_char = self.buffer[self.buffer_pointer]
+        self.buffer_pointer += 1
+
+        if next_char == '\n':
+            self.line_no += 1
+
+        return next_char
+    
+    
